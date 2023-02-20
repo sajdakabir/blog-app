d9bc53e7d72a7ed64a05e496508dc7826dc581ab
sajda <sajda.kbir@gmail.com>
2023-02-21 00:05:43 +0530

    added navbar and post section

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,45 @@ import './App.css';
 
 function App() {
   return (
-  <div>
-    Hello
-  </div>
+    <main>
+      <header>
+        <a href="" className="logo">MyBlog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <div className="image">
+          <img src="https://mywowo.net/media/images/cache/dubai_img_worlds_of_adventure_01_presentazione_jpg_1200_630_cover_85.jpg" />
+        </div>
+
+        <div className="texts">
+          <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+          <p className="info">
+            <a className="author">Sajda Kabir</a>
+            <time>2023-2-20 23:53</time>
+
+          </p>
+          <p className="summary">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus quaerat eius expedita cum, nobis a velit omnis repellat quas consectetur ut vitae quos.</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src="https://mywowo.net/media/images/cache/dubai_img_worlds_of_adventure_01_presentazione_jpg_1200_630_cover_85.jpg" />
+        </div>
+
+        <div className="texts">
+          <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+          <p className="info">
+            <a className="author">Sajda Kabir</a>
+            <time>2023-2-20 23:53</time>
+
+          </p>
+          <p className="summary">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus quaerat eius expedita cum, nobis a velit omnis repellat quas consectetur ut vitae quos.</p>
+        </div>
+      </div>
+    </main>
   );
 }
 
