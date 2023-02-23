5192f0da7bc9d5fd88c1d70ef1d437f1bc53d3fb
sajda <sajda.kbir@gmail.com>
2023-02-23 20:59:49 +0530

    added model for user

---
+++ server/models/User.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const UserSchema=new Schema({
+    username:{
+        type:String,
+        required:true,
+        min:4,
+        unique:true
+
+    },
+    password:{
+        type:String,
+        required:true
+
+    },
+});
+
+const UserModel=mongoose.model('User',UserSchema);
+
+module.exports=UserModel;
