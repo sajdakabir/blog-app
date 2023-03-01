117a4e55515db8c3418de64396e5317ead16e2eb
sajda <sajda.kbir@gmail.com>
2023-03-01 23:55:15 +0530

    model and users

--- server/index.js
+++ server/index.js
@@ -1,17 +1,25 @@
 import expess from 'express';
 import cors from 'cors';
+import connectDB from './db/connect.js';
+import dotenv from 'dotenv';
+import User from './models/User.js'
 
 const app=expess();
 
 
 app.use(cors());
 app.use(expess.json());
+dotenv.config();
 
-app.post('/register',(req,res)=>{
+connectDB();
+
+app.post('/register',async(req,res)=>{
     const {username,password}=req.body;
-    res.json({result:{username,password}})
+    const userInfo=await User.create({username,password})
+    res.json(userInfo);
 })
+const port=process.env.PORT || 5000; 
 
-app.listen(5000,(req,res)=>{
-    console.log("server running on post 5000");
+app.listen(port,(req,res)=>{
+    console.log(`server running on post ${port}`);
 })
--- server/models/User.js
+++ server/models/User.js
@@ -18,4 +18,5 @@ const UserSchema=new Schema({
 
 const UserModel=mongoose.model('User',UserSchema);
 
-module.exports=UserModel;
+
+export default UserModel;
