6b5d60f66d47af4de89819f7259d94de2e5b171d
sajda <sajda.kbir@gmail.com>
2023-03-03 01:14:38 +0530

    make the password encrypted using bcrypt

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
 import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
 
+const salt=process.env.SALT;
 
 export const register = async (req, res) => {
     const { username, password } = req.body;
     try {
-        const userInfo = await User.create({ username, password });
+        const userInfo = await User.create({ 
+            username,
+             password:bcrypt.hashSync(password,salt),
+             });
         res.json(userInfo);
     } catch (error) {
         res.status(400).json(error);
