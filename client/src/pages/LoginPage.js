f5ec03858699173ce2aa3c70131b383560aee582
sajda <sajda.kbir@gmail.com>
2023-03-04 23:46:53 +0530

    login done from the front-end part

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,15 +1,27 @@
-import { useState } from "react"
+import { useState } from "react";
+import {Navigate} from 'react-router-dom';
 
 export default function LoginPage() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [redirect,setRedirect]=useState(false);
     const login= async(event)=>{
         event.preventDefault();
         const result=await fetch('http://localhost:5000/login',{
             method:'POST',
             body:JSON.stringify({username,password}),
             headers: {'Content-Type':'application/json'},
-        })
+            credentials:'include', //include the cookie
+        });
+        if(result.ok){
+            setRedirect(true);
+        }else{
+            alert('Wrong credentials!')
+        }
+        
+    }
+    if(redirect){
+        return <Navigate to={'/'} />
     }
     return (
         <form className="login" onSubmit={login}>
