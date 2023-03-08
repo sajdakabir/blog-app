ed65a48fdaf5622141a40704b84d99b1919a51d4
sajda <sajda.kbir@gmail.com>
2023-03-08 19:08:49 +0530

    added profile api

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,23 +1,39 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Header.css';
-import { Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Header() {
+  const [username, setUserName] = useState(null);
 
-  useEffect(()=>{
-    fetch('http://localhost:3000/profile',{
-      credentials:'include',
-    })
+  useEffect(() => {
+    fetch('http://localhost:5000/profile', {
+      credentials: 'include',
+    }).then(res => {
+      res.json().then(user => {
+        setUserName(user.username);
+      });
+    });
 
-  },[]);
+  }, []);
   return (
     <header>
-        <Link to="/" className="logo">MyBlog</Link>
-        <nav>
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
-        </nav>
-      </header>
+      <Link to="/" className="logo">MyBlog</Link>
+      <nav>
+        {username && (
+          <>
+          <Link to='/create'>Create new post</Link>
+          <a>Logout</a>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+
+      </nav>
+    </header>
   )
 }
 
